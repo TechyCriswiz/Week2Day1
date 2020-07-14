d795d747398a064b2ed45288ead1581c8bd6cf26
TechyCriswiz <67975057+TechyCriswiz@users.noreply.github.com>
2020-07-14 14:45:53 -0500

    Add files via upload

---
+++ Cristian_S_OperatorExcercise.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Calculator Input
+var inputOne = 173
+var inputTwo = 50
+
+// Calculator Operations
+let addition = inputOne + inputTwo
+let subtraction = inputOne - inputTwo
+let multiplication = inputOne * inputTwo
+let division = inputOne / inputTwo
+
+// Outputs
+print ("Your Variables Are \(inputOne) and \(inputTwo)\n")
+print ("Addition Answer: \(addition)\n")
+print ("Subtraction Answer: \(subtraction)\n")
+print ("Multiplication Answer: \(multiplication)\n")
+print ("Division Answer: \(division)\n")
